b804cf3a5b196926dbb0c91a5d8109bee1008627
yihongsoon <soonyh-pm18@student.tarc.edu.my>
2021-08-04 20:20:07 +0800

    Sidebar

--- DMS/dmsMasterpage.Master.designer.cs
+++ DMS/dmsMasterpage.Master.designer.cs
@@ -42,6 +42,15 @@ namespace DMS
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
 
         /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
         /// btnViewProfile control.
         /// </summary>
         /// <remarks>
