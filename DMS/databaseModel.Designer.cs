d9b282f2f7961eaf7681c512d7ab8e2ccf994d7d
kelvinldq-pm18@student.tarc.edu.my <kelvinldq-pm18@student.tarc.edu.my>
2021-08-13 23:08:52 +0800

    no changes

--- DMS/databaseModel.Designer.cs
+++ DMS/databaseModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\yihon\source\repos\DMS\DMS\databaseModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\User\source\repos\DMS\DMS\databaseModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
